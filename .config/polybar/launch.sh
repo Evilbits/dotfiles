97e44d0fc3a2759394379bec4549924d3f4e7f17
Rasmus <562749+Evilbits@users.noreply.github.com>
2020-03-12 09:47:31 +0700

    Added new polybar and picom as compositor. Small changes here and there as well.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,12 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top -c ~/.config/polybar/config-top.ini &
-    MONITOR=$m polybar --reload bottom -c ~/.config/polybar/config-bottom.ini &
-  done
-else
-  polybar --reload top -c ~/.config/polybar/config-top.ini &
-  polybar --reload bottom -c ~/.config/polybar/config-bottom.ini &
-fi
+polybar -c ~/.config/polybar/config.ini main &
