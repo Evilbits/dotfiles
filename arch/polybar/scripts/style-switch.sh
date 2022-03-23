8a44b402e4cadac3e494d6bcb7fed32299232816
Rasmus <562749+Evilbits@users.noreply.github.com>
2022-03-23 19:08:10 +0100

    Changes

--- arch/polybar/scripts/style-switch.sh
+++ arch/polybar/scripts/style-switch.sh
@@ -5,7 +5,7 @@
 
 SDIR="$HOME/.config/polybar/scripts"
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Select' -location 5 -columns 1 -xoffset -350 -yoffset -40 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< "> Feather|> Material|> Siji|> Typicons")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Select' -location 3 -columns 1 -xoffset -270 -yoffset 40 -width 10 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< "> Feather|> Material|> Siji|> Typicons")"
             case "$MENU" in
 				## Light Colors
 				*Feather) $SDIR/style.sh -Feather ;;
