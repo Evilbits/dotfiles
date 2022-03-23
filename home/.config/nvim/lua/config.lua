934ea5caa3ee5956f4b232b678b53276431d5364
Rasmus <rasmus.reiler@lenus.io>
2022-03-23 18:06:42 +0100

    New stuff

--- home/.config/nvim/lua/config.lua
+++ home/.config/nvim/lua/config.lua
@@ -93,46 +93,22 @@ require'lspconfig'.tsserver.setup{}
 
 -- Setup function signatures
 require'lsp_signature'.setup({
-  debug = false, -- set to true to enable debug logging
-  log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
-  verbose = false, -- show debug line number
-
   bind = true, -- This is mandatory, otherwise border config won't get registered.
-  doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-
-  floating_window_above_cur_line = true, -- try to place the floating above the current line when possible.
-
-  floating_window_off_x = 1, -- adjust float windows x position.
-  floating_window_off_y = 1, -- adjust float windows y position.
-
-
-  fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
-  hint_enable = false, -- virtual hint enable
-  max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-                   -- to view the hiding contents
-  --max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
   handler_opts = {
     border = "rounded"   -- double, rounded, single, shadow, none
   },
-
-  always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-
-  auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
-  extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-
-  padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
-
-  transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-  shadow_blend = 36, -- if you using shadow as border use this set the opacity
-  shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-  timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = nil -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 })
 
 -- Lualine
-require('evil_lualine')
+--require('evil_lualine')
+require('lualine').setup({ 
+  options = { transparent = true },
+  sections = {
+    lualine_b = {'diff', 'diagnostics'},
+  }
+})
 
+-- Treesitter
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
@@ -163,11 +139,11 @@ require('telescope').setup {
     },
   }
 }
+require('telescope').load_extension('fzf')
 
 -- Trouble setup
 require('trouble').setup {}
 
-require('telescope').load_extension('fzf')
 local theme = require('telescope.themes').get_dropdown({})
 -- Search keybinds
 vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>lua require("telescope.builtin").find_files({hidden = true})<CR>', {})
@@ -179,6 +155,7 @@ vim.api.nvim_set_keymap('n', '<leader>gD', '<cmd>lua require("telescope.builtin"
 vim.api.nvim_set_keymap('n', '<leader>gc', ':Trouble document_diagnostics<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>gi', '<cmd>lua require("telescope.builtin").lsp_implementations()<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>gt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<CR>', {})
+vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
 -- Git keybinds
 vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>lua require("telescope.builtin").git_status()<CR>', {})
 -- Treesitter keybinds
