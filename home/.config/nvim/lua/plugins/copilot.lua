611d4b3e30c364950d81e6fd36b036f93c30759f
Rasmus Reiler <562749+Evilbits@users.noreply.github.com>
2024-12-02 11:55:02 +0000

    Update copilotchat

--- home/.config/nvim/lua/plugins/copilot.lua
+++ home/.config/nvim/lua/plugins/copilot.lua
@@ -18,14 +18,15 @@ return {
   { 'github/copilot.vim' },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     build = "make tiktoken", -- Only on MacOS or Linux
+    branch = "main",
     opts = {
       debug = true, -- Enable debugging
+      model = "claude-3.5-sonnet",
       -- See Configuration section for rest
       prompts = {},
     },
