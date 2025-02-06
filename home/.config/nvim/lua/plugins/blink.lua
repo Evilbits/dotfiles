6ad187e9b538e0fc917a084b76ea79e22aa40610
Rasmus Reiler <562749+Evilbits@users.noreply.github.com>
2025-02-06 13:59:09 +0000

    Lazy update

--- home/.config/nvim/lua/plugins/blink.lua
+++ home/.config/nvim/lua/plugins/blink.lua
@@ -6,19 +6,12 @@ return {
   -- use a release tag to download pre-built binaries
   version = 'v0.*',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
     keymap = {
-      preset    = 'default',
+      preset    = 'enter',
       ['<C-k>'] = { 'select_prev', 'fallback' },
       ['<C-j>'] = { 'select_next', 'fallback' },
-      ['<CR>']  = { 'select_and_accept', 'fallback' },
+      -- ['<CR>']  = { 'select_and_accept', 'fallback' },
     },
     completion = {
       accept = {
@@ -27,13 +20,17 @@ return {
         },
       },
       list = {
-        --selection = 'auto_insert'
-        selection = 'preselect'
+        selection = {
+          -- preselect = true,
+          preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
+          auto_insert = function(ctx) return ctx.mode == 'cmdline' end,
+        },
       },
       menu = {
+        -- Don't show for cmdline
         border = 'single',
         draw = {
-          treesitter = true,
+          treesitter = { 'lsp' },
           components = {
             label_description = {
               width = { min = 20, max = 20 },
